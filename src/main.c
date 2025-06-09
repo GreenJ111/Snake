ded4149768ab847bd17bab41de676db58445a919
Jake <Greenwood.H.Jake@gmail.com>
2025-06-09 16:09:38 +0100

    Added close window input

--- src/main.c
+++ src/main.c
@@ -11,10 +11,22 @@ int main(int argc, char* argv[]) {
     SDL_Window* window = SDL_CreateWindow("Snake Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, 0);
     SDL_Surface* surface = SDL_GetWindowSurface(window);
     SDL_Rect rect = {200,200,50,50};
+    SDL_Event event;
 
-    SDL_FillRect(surface, &rect, 0xffffffff);
-    SDL_UpdateWindowSurface(window);
+    int game = 1;
+    while (game == 1) {
+        SDL_FillRect(surface, NULL, 0x00000000);
 
-    SDL_Delay(5000);
+        while(SDL_PollEvent(&event)){
+            switch (event.type){
+                case(SDL_QUIT):
+                game=0;
+                break;
+            }
+        
+            SDL_FillRect(surface, &rect, 0xffffffff);
+            SDL_UpdateWindowSurface(window);
+        };
+    };
     return 0;
-}
+};
